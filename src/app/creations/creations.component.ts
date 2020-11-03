fa2c18c1899120f118f6ff9f75121213fc4abb79
jgullung <jgullung@klanik.com>
2020-11-03 22:00:38 +0100

    [UPDATE] curator.io loading at first time + "send" to "envoyer"

--- src/app/creations/creations.component.ts
+++ src/app/creations/creations.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-creations',
-  templateUrl: './creations.component.html',
-  styleUrls: ['./creations.component.scss']
+  selector: "app-creations",
+  templateUrl: "./creations.component.html",
+  styleUrls: ["./creations.component.scss"],
 })
 export class CreationsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    var i,
+      e,
+      d = document,
+      s = "script";
+    i = d.createElement("script");
+    i.async = 1;
+    i.src =
+      "https://cdn.curator.io/published/27c72be9-3dd0-4408-8763-ac264862c68c.js";
+    e = d.getElementsByTagName(s)[0];
+    e.parentNode.insertBefore(i, e);
   }
-
 }
